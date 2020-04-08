4d962903b7cbf75f6922cc432a6891710c8e82cd
Florian Le Minoux <florian.le-minoux@insa-rennes.fr>
2020-04-08 15:21:22 +0200

    feat: adding an account listing method

--- src/micro-auth/rest-api.go
+++ src/micro-auth/rest-api.go
@@ -301,6 +301,80 @@ func createAccount(w http.ResponseWriter, r *http.Request) {
 	w.Write(m)
 }
 
+func listAccounts(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var reqData VerifyRequest
+	unmarshalErr := json.Unmarshal(reqBody, &reqData)
+	if unmarshalErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("[ERROR] Unmarshal request json failed: %v", unmarshalErr.Error())
+		w.Write([]byte("[MICRO-AUTH] Wrong request body format"))
+		return
+	}
+
+	claims, checkingErr := checkToken(reqData.Token)
+
+	if checkingErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(checkingErr.Error()))
+	}
+
+	if claims.Role != "0" { // Creator needs to be administrator
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("[MICRO-AUTH] Insufficient permissions to list accounts"))
+	}
+
+	selectStatement, selectErr := Db.Prepare("SELECT username, role FROM users")
+
+	if selectErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("[ERROR] Error while preparing select request (listAccounts): %v", selectErr.Error())
+		w.Write([]byte("[MICRO-AUTH] Could not prepare request"))
+		return
+	}
+
+	defer selectStatement.Close()
+
+	rows, queryErr := selectStatement.Query()
+
+	if queryErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("[ERROR] Error while executing select request (listAccounts): %v", queryErr.Error())
+		w.Write([]byte("[MICRO-AUTH] Could not executing request"))
+		return
+	}
+
+	accounts := make([]AccountData, 0)
+
+	for rows.Next() {
+
+		var username string
+		var role string
+
+		if err := rows.Scan(&username, &role); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("[ERROR] Error while scanning row (listAccounts): %v", queryErr.Error())
+			w.Write([]byte("[MICRO-AUTH] Could not read database"))
+			return
+		}
+
+		accounts = append(accounts, AccountData{
+			Username: username,
+			Role:     role,
+		})
+
+	}
+
+	m, _ :=json.Marshal(accounts)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(m)
+}
 
 func deleteAccount(w http.ResponseWriter, r *http.Request) {
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -442,6 +516,7 @@ func main() {
 	router.HandleFunc("/auth/", home)
 	router.HandleFunc("/auth/login", login).Methods("POST")
 	router.HandleFunc("/auth/logout", logout).Methods("POST")
+	router.HandleFunc("/auth/account/list", listAccounts).Methods("POST")
 	router.HandleFunc("/auth/account/create", createAccount).Methods("POST")
 	router.HandleFunc("/auth/account/delete", deleteAccount).Methods("DELETE")
 	router.HandleFunc("/auth/verifyToken", verify).Methods("POST")
