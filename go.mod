4a254548e094e12ef9e0f82cbedf8535736ffca1
Florian Le Minoux <florian.le-minoux@insa-rennes.fr>
2020-05-08 23:53:37 +0200

    test: cover verify cases & prevent concurrency issues

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module auth
 go 1.14
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
-	github.com/prometheus/client_golang v1.6.0 // indirect
-	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.7.4
+	github.com/prometheus/client_golang v1.6.0
+	github.com/stretchr/testify v1.5.1
 	github.com/taliesin-insa/lib-auth v0.0.0-20200419103633-e908aae48af9
 )
--- src/micro-auth/API_test.go
+++ src/micro-auth/API_test.go
@@ -4,19 +4,24 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 )
+
+var TestId string
 
 func setupMockDbData() {
 	_, insertErr := Db.Exec("INSERT INTO users VALUES" +
-		"('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 0,'admin@root.fr'), " +
-		"('user','04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb', 1,'user@root.fr')")
+		"('admin"+TestId+"','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 0,'admin@root.fr'), " +
+		"('user"+TestId+"','04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb', 1,'user@root.fr')")
 
 	if insertErr != nil {
 		panic(insertErr.Error())
@@ -25,13 +30,19 @@ func setupMockDbData() {
 
 func cleanupDb() {
 	_, deleteErr := Db.Exec("DELETE FROM users")
+	_, deleteSessionsErr := Db.Exec("DELETE FROM sessions")
 
-	if deleteErr != nil {
+	if deleteErr != nil || deleteSessionsErr != nil {
 		panic(deleteErr.Error())
 	}
 }
 
 func TestMain(m *testing.M) {
+	// generate a unique identifier for values inserted in db
+	// to prevent concurrency issues
+	now := time.Now()
+	nsec := now.UnixNano()
+	TestId = strconv.FormatInt(nsec, 10)
 
 	var passwordValue string
 
@@ -59,10 +70,10 @@ func TestMain(m *testing.M) {
 
 }
 
-func TestSuccessfulLogin(t *testing.T) {
+func _login(t *testing.T, username string, password string) *httptest.ResponseRecorder {
 	requestPayload := AuthRequest{
-		Username: "admin",
-		Password: "admin",
+		Username: username,
+		Password: password,
 	}
 
 	jsonData, jsonErr := json.Marshal(&requestPayload)
@@ -78,6 +89,13 @@ func TestSuccessfulLogin(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	login(recorder, request)
+
+	return recorder
+}
+
+func TestSuccessfulLogin(t *testing.T) {
+
+	recorder := _login(t, "admin"+TestId, "admin")
 
 	assert.Equal(t, http.StatusOK, recorder.Code)
 	responseBody := recorder.Body.Bytes()
@@ -85,39 +103,35 @@ func TestSuccessfulLogin(t *testing.T) {
 
 	json.Unmarshal(responseBody, &response)
 
-	assert.Equal(t, "admin", response.Username)
+	assert.Equal(t, "admin"+TestId, response.Username)
 	assert.Equal(t, "admin@root.fr", response.Email)
 	assert.Equal(t, 0, response.Role)
 
 }
 
 func TestLoginWrongPassword(t *testing.T) {
-	requestPayload := AuthRequest{
-		Username: "admin",
-		Password: "non",
-	}
-
-	jsonData, jsonErr := json.Marshal(&requestPayload)
-	if jsonErr != nil {
-		t.Fatal(jsonErr.Error())
-	}
-
-	request := &http.Request{
-		Method: http.MethodPost,
-		Body: ioutil.NopCloser(bytes.NewBuffer(jsonData)),
-	}
-
-	recorder := httptest.NewRecorder()
-
-	login(recorder, request)
+	recorder := _login(t, "admin"+TestId, "admin")
 
 	assert.Equal(t, http.StatusUnauthorized, recorder.Code)
 }
 
 func TestLoginNonExistingUser(t *testing.T) {
-	requestPayload := AuthRequest{
-		Username: "toto",
-		Password: "ah",
+	recorder := _login(t, "null", "admin")
+
+	assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+}
+
+func TestVerifyValidToken(t *testing.T) {
+	loginRecorder := _login(t, "admin"+TestId, "admin")
+	assert.Equal(t, http.StatusOK, loginRecorder.Code)
+
+	responseBody := loginRecorder.Body.Bytes()
+	loginResponse := AuthResponse{}
+
+	json.Unmarshal(responseBody, &loginResponse)
+
+	requestPayload := VerifyRequest{
+		Token: loginResponse.Token,
 	}
 
 	jsonData, jsonErr := json.Marshal(&requestPayload)
@@ -130,10 +144,85 @@ func TestLoginNonExistingUser(t *testing.T) {
 		Body: ioutil.NopCloser(bytes.NewBuffer(jsonData)),
 	}
 
-	recorder := httptest.NewRecorder()
+	checkRecorder := httptest.NewRecorder()
 
-	login(recorder, request)
+	verify(checkRecorder, request)
 
-	assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+	assert.Equal(t, http.StatusOK, checkRecorder.Code)
+}
 
+func TestVerifyInvalidToken(t *testing.T) {
+	loginRecorder := _login(t, "admin"+TestId, "admin")
+	assert.Equal(t, http.StatusOK, loginRecorder.Code)
+
+	responseBody := loginRecorder.Body.Bytes()
+	loginResponse := AuthResponse{}
+
+	json.Unmarshal(responseBody, &loginResponse)
+
+	requestPayload := VerifyRequest{
+		Token: "bad_token",
+	}
+
+	jsonData, jsonErr := json.Marshal(&requestPayload)
+	if jsonErr != nil {
+		t.Fatal(jsonErr.Error())
+	}
+
+	request := &http.Request{
+		Method: http.MethodPost,
+		Body: ioutil.NopCloser(bytes.NewBuffer(jsonData)),
+	}
+
+	checkRecorder := httptest.NewRecorder()
+
+	verify(checkRecorder, request)
+
+	assert.Equal(t, http.StatusOK, checkRecorder.Code)
+}
+
+func _generateValidToken(t *testing.T, username string, role int, email string) string {
+
+	stdclaims := jwt.StandardClaims{
+		IssuedAt: time.Now().Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, JwtClaims{
+		Username: username,
+		Role: role,
+		Email: email,
+		StandardClaims: stdclaims,
+	})
+
+	tokenString, signingErr := token.SignedString(HMACSecret)
+
+	if signingErr != nil {
+		t.Fatal(signingErr)
+	}
+
+	return tokenString
+}
+
+func TestVerifyValidTokenNotInSessions(t *testing.T) {
+	token := _generateValidToken(t, "admin"+TestId, RoleAdmin, "admin@root.fr")
+
+	requestPayload := VerifyRequest{
+		Token: token,
+	}
+
+	jsonData, jsonErr := json.Marshal(&requestPayload)
+	if jsonErr != nil {
+		t.Fatal(jsonErr.Error())
+	}
+
+	request := &http.Request{
+		Method: http.MethodPost,
+		Body: ioutil.NopCloser(bytes.NewBuffer(jsonData)),
+	}
+
+	checkRecorder := httptest.NewRecorder()
+
+	verify(checkRecorder, request)
+
+	assert.Equal(t, http.StatusUnauthorized, checkRecorder.Code)
 }
